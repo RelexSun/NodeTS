6e554c2ec13f820d0697d72f4623f32b5eaa8908
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-02-21 17:23:14 +0700

    register

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,1 +1,6 @@
 import express from "express";
+import { register } from "../controllers/authController";
+
+export default (router: express.Router) => {
+  router.post("/auth/register", register);
+};

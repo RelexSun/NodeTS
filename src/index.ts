2c99d56de824b6ed17406a60d962150869da1594
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-02-21 09:25:31 +0700

    start 🔐

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+const port = 8080;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+const MONGO_URL = "mongodb+srv://sun:12345@cluster0.j46fme6.mongodb.net/";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
